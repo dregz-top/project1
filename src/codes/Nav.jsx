1fdf842c94f315fbd2b65046c714c08f723a44b4
dregz-top <adhamelawamy59@gmail.com>
2025-09-24 18:55:16 +0300

    Update Nav.jsx

--- src/codes/Nav.jsx
+++ src/codes/Nav.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import trone from '/photos/trone.png'
 function Nav() {
   const [SEARCH, SETSEARCH] = useState("");
   const [DATA, SETDATA] = useState([]);
